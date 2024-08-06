1b45b9f3108d4c05ad71948bc50e5765cc91da99
yasminaa18 <yasminaltino8@gmail.com>
2024-08-06 13:43:17 -0300

    Mais comentários

--- binaria.h
+++ binaria.h
@@ -6,16 +6,22 @@
 #include <stdbool.h>
 #include "struct.h"
 
+// Estrutura básica da árvore
 typedef struct{
     Registro dados;
     int esq, dir;
 }tNo;
 
+// Função que cria um novo nó
 tNo criaNovoNo(Registro dados);
+
+// Função que gera a árvore binária em um arquivo
 bool geraArquivoBinaria(FILE* origem, int quantidade, TipoContador*);
+
+// Função que insere registros na árvore
 bool insere(Registro dado, int qtdItens, FILE* arvore, TipoContador*);
-void imprimeArvore(FILE*, int);
-int buscaBinaria(FILE* arvore, int posAtual, tNo no, int chave, Registro *dado, long int *leit, int depth);
+
+// Função que realiza uma busca binária iterativa em uma árvore armazenada em um arquivo
 int buscaBinariaI(FILE* arvore, int posAtual, tNo no, int chave, Registro *dado, TipoContador *cont);
 
 #endif
